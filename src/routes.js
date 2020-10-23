1725ac453ab23f380ff109799c5b58fccf83179f
GuilhermeVitorino <gui_vito@hotmail.com>
2020-10-22 21:01:06 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+const routes = require('express').Router();
+const express = require('express');
+const multer = require('multer');
+const multerConfig = require("./config/multer");
+
+const Post = require('./models/Post');
+
+routes.get("/posts", async (req, res) => {
+    const posts = await Post.find();
+
+    return res.json(posts);
+})
+
+routes.post("/posts", multer(multerConfig).single('file'), async (req, res) => {
+
+    const { originalname: name, size, key, location: url} = req.file;
+    const { restriction } = req.body;
+
+    const post = await Post.create({
+        name,
+        restriction,
+        size,
+        key,
+        url
+    });
+
+    return res.json(post);
+});
+
+routes.delete('/posts/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    await post.remove();
+
+    return res.send();
+})
+
+routes.get('/posts/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    return res.json(post)
+})
+
+module.exports = routes;
